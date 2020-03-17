d30d2ac780299a5cec46b7e20515a137fdcf4b87
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-03-17 19:53:37 +0000

    initial constructor

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,2 +1,17 @@
 public class Matrix {
+    private int row;
+    private int col;
+    private int [][] matrix;
+
+    // populate a zero n x m array
+    public Matrix(int row, int col)
+    {
+        this.matrix = new int[row][col];
+
+    }
+    public Matrix(int [][] matrix)
+    {
+        this.matrix = matrix;
+    }
+
 }
--- src/MatrixMain.java
+++ src/MatrixMain.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+
 public class MatrixMain {
+    public static void main(String[] args) {
+
+    }
 }
